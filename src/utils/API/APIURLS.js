2a489f692b60466ccf484e0980f1acce45fcd152
Mateusz Bednarz <bednarz.mateusz@gmail.com>
2023-12-19 17:14:45 +0100

    added files with sample API responses to public; added file with URLS for API requests

---
+++ src/utils/API/APIURLS.js
@@ -0,0 +1,20 @@
+export const URL = "https://api.themoviedb.org/3/";
+export const apiKey = "f9cdeaf9af4ca30fda858745e2b12712";
+
+export const popularMoviesURL = `${URL}movie/popular?api_key=${apiKey}&language=en-US&page=1`;
+
+export const popularPeopleURL = `${URL}person/popular?api_key=${apiKey}&language=en-US&page=1`;
+
+export const genresURL = `${URL}genre/movie/list?api_key=${apiKey}&language=en-US`;
+
+export const backdropURL = "https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/";
+
+//tymczasowe linki do kopiowania:
+    // link to pierwszej strony popularMovies
+        //      https://api.themoviedb.org/3/movie/popular?api_key=f9cdeaf9af4ca30fda858745e2b12712&language=en-US&page=1
+
+    //link to pierwszej strony popularPeople
+        //      https://api.themoviedb.org/3/person/popular?api_key=f9cdeaf9af4ca30fda858745e2b12712&language=en-US&page=1
+
+    //początek linku do backdrop image
+        //  https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/
