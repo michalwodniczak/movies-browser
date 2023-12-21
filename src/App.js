b5131191c0ab21a1292ba75a8ec1b4c57d597db8
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-12-21 15:52:33 +0100

    chnaged paths ThemeProvider and GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import { ThemeProvider } from "styled-components";
-import { theme } from "./app/theme";
-import { GlobalStyle } from "./app/GlobalStyle";
-
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
       <header>
         <h1>Movie Browser</h1>
       </header>
-    </ThemeProvider>
-  )
-}
+  );
+};
 
 export default App;
