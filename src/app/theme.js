fa3f0210157005b7fb98ae6d9b806650f3a2e7ff
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-12-22 15:36:10 +0100

    changed name color

--- src/app/theme.js
+++ src/app/theme.js
@@ -1,11 +1,12 @@
 export const theme = {
     color: {
         white: "#FFFFFF",
-        whisper: "#F5F5FA",
-        mystic: "#E4E6F0",
-        lightBlue: "#D6E4FF",
-        gray: "#7E839A",
-        lightBlack: "#18181B",
+        lightGray: "#F5F5FA",
+        gray: "#E4E6F0",
+        darkGray: "#7E839A",
+        almostBlack: "#18181B",
         black: "#000000",
+        lightBlue: "#D6E4FF",
+        blue: "#0044CC",
     }
 };
