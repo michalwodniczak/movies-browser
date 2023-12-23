cc3c2bc9cd226de7f273541f89a6b5888ae54deb
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-12-23 18:13:06 +0100

    create content for MovieList component to display popular movies using data from Redux state

--- src/features/movies/MovieList/index.js
+++ src/features/movies/MovieList/index.js
@@ -1,7 +1,30 @@
+import { useSelector } from "react-redux";
+import { selectMovieList } from "./movieListSlice";
+
 function MovieList() {
+    const popularMovies = useSelector(selectMovieList);
+
     return (
-        ""
-    )
+        <>
+            <div>
+                <h1>Popular Movies</h1>
+                <ul>
+                    {popularMovies.map((movie) => (
+                        <li key={movie.id}>
+                            <div
+                                id={movie.id}
+                                poster={movie.poster_path}
+                                title={movie.title}
+                                vote={movie.vote_average}
+                                votes={movie.vote_count}
+                            />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            {/* <Pagination /> */}
+        </>
+    );
 };
 
 export default MovieList;
