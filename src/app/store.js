b5aa155dbc67d5c87deb844c2d781b74e458139a
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-12-24 15:35:09 +0100

    integrate sagaMiddleware and movie list functionality

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
+import paginationReducer from "./kopiePagination";
+import { watchFetchMovieList } from '../features/movies/MovieList/movieListSaga';
+import movieListReducer from '../features/movies/MovieList/movieListSlice';
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
-
+    reducer: {
+        pagination: paginationReducer,
+        movieList: movieListReducer,
+    },
+    middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(watchFetchMovieList);
+
+export default store;
