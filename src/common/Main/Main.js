005dba8898aafc0cef5638b2c9eb9137de417e14
Mateusz Bednarz <bednarz.mateusz@gmail.com>
2023-12-25 12:11:47 +0100

    added side padding on narrower screens to Main

--- src/common/Main/Main.js
+++ src/common/Main/Main.js
@@ -4,4 +4,9 @@ export const Main = styled.main`
 	max-width: 1368px;
     margin: auto;
     padding-top: 64px;
+
+    @media (max-width: 1400px) {
+		padding-left: 16px;
+        padding-right: 16px;
+	}
 `
