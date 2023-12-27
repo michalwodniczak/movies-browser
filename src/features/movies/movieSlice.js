ce207b3df5d83762604d7a0c9f39a069d026fd55
Mateusz Bednarz <bednarz.mateusz@gmail.com>
2023-12-27 16:40:06 +0100

    created movieSlice to store movieDetails

---
+++ src/features/movies/movieSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const movieSlice = createSlice({
+    name: 'movie',
+    initialState: {
+        id: '',
+        details: {},
+        credits: {},
+    },
+    reducers: {
+        setMovieId: (state, {payload: id}) => {
+            state.id = id;
+        },
+        setMovieDetails: (state, {payload: movieDetails}) => {
+            state.details = movieDetails;
+        },
+        setMovieCredits: (state, {payload: movieCredits}) => {
+            state.details = movieCredits;
+        },
+    },
+});
+
+export const {
+    setMovieId,
+    setMovieDetails,
+    setMovieCredits,
+} = movieSlice.actions;
+
+const selectMovieState = state => state.movie;
+
+export const selectMovieId = state => selectMovieState(state).id;
+
+export default movieSlice.reducer;
