e25b687809c52a7886fe5328725786e4f47b73aa
Mateusz Bednarz <bednarz.mateusz@gmail.com>
2023-12-29 11:25:47 +0100

    added draft structure to PeopleList

--- src/features/people/PeopleList/index.js
+++ src/features/people/PeopleList/index.js
@@ -1,7 +1,23 @@
-function PersonList() {
-    return (
-        ""
-    )
+import { Section, SectionTitle } from "../../../common/Section/Section";
+import { SectionWrapper } from "./styled";
+import Pagination from '../../../common/Pagination';
+
+function PeopleList() {
+
+  return (
+    <>
+      <Section>
+        <SectionTitle>
+          Popular People
+        </SectionTitle>
+        <SectionWrapper>
+
+
+        </SectionWrapper>
+      </Section>
+      <Pagination />
+    </>
+  )
 };
 
-export default PersonList;
+export default PeopleList;
