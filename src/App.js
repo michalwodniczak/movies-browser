ae881f10ae1a658f992d0b1aa10584791d0275ce
Mateusz Bednarz <bednarz.mateusz@gmail.com>
2023-12-29 11:29:21 +0100

    added Main component and enclosed Switch in it

--- src/App.js
+++ src/App.js
@@ -5,30 +5,31 @@ import MovieDetails from "./features/movies/MovieDetails";
 import PeopleList from "./features/people/PeopleList";
 import PersonDetails from "./features/people/PeopleDetails";
 import { toMovieList, toMovieDetails, toPeopleList, toPeopleDetails } from "./routes";
+import { Main } from './common/Main/Main';
 
 function App() {
   return (
     <HashRouter>
       <Navigation />
-
-      <Switch>
-        <Route path={toMovieDetails()}>
-          <MovieDetails />
-        </Route>
-        <Route path={toMovieList()}>
-          <MovieList />
-        </Route>
-        <Route path={toPeopleDetails()}>
-          <PersonDetails />
-        </Route>
-        <Route path={toPeopleList()}>
-          <PeopleList />
-        </Route>
-        <Route path='/'>
-          <Redirect to={toMovieList()} />
-        </Route>
-      </Switch>
-
+      <Main>
+        <Switch>
+          <Route path={toMovieDetails()}>
+            <MovieDetails />
+          </Route>
+          <Route path={toMovieList()}>
+            <MovieList />
+          </Route>
+          <Route path={toPeopleDetails()}>
+            <PersonDetails />
+          </Route>
+          <Route path={toPeopleList()}>
+            <PeopleList />
+          </Route>
+          <Route path='/'>
+            <Redirect to={toMovieList()} />
+          </Route>
+        </Switch>
+      </Main>
     </HashRouter>
 
   );
---
+++ src/common/Main/Main.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Main = styled.main`
+	max-width: 1368px;
+    margin: auto;
+    padding-top: 64px;
+    padding-bottom: 103px;
+
+    @media (max-width: 1400px) {
+		padding-left: 16px;
+        padding-right: 16px;
+	}
+`
