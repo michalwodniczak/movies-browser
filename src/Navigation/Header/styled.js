c91c9ea285258cdee805f07ecc0a8acf4811cd88
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-12-30 11:26:41 +0100

    added styles to pseudo-classes in NavLink

--- src/Navigation/Header/styled.js
+++ src/Navigation/Header/styled.js
@@ -61,25 +61,24 @@ export const StyledNavLink = styled(NavLink)`
     text-decoration: none;
     text-align: center;
     padding: 13px 24px;
+    border-radius:24px;
     transition: 0.3s;
 
     &:visited{
-        color: ${({theme}) => theme.breakpoints.white};
+        color: ${({theme}) => theme.color.white};
     };
 
     &:focus{
-        transform: scale(0.5);
+        color: ${({theme}) => theme.color.blue};
     };
 
     &:hover{
-        transform: scale(0.5);
+        color: ${({theme}) => theme.color.blue};
     };
 
-    
     &.active{
-        color: ${({ theme }) => theme.color.white};
+        color: ${({ theme }) => theme.color.white}; 
         border: 1px solid ${({ theme }) => theme.color.white};
-        border-radius: 24px;
     };
 
     @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px){
