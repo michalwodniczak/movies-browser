2c60e82ec1ca0ea7b7b7a450bf134cf7a23ec0a8
Mateusz Bednarz <bednarz.mateusz@gmail.com>
2023-12-31 19:12:19 +0100

    styling with bespoke tiles for now

--- src/features/movies/MovieList/index.js
+++ src/features/movies/MovieList/index.js
@@ -2,79 +2,97 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useLocation, useHistory, Link } from "react-router-dom";
 import {
-    incrementPage,
-    decrementPage,
-    goToFirstPage,
-    pageNumberFromURL,
-    selectMovieList,
-    selectLoading,
-    selectPageState
+	incrementPage,
+	decrementPage,
+	goToFirstPage,
+	pageNumberFromURL,
+	selectMovieList,
+	selectLoading,
+	selectPageState
 } from "./movieListSlice";
+import { posterURL } from "../../../utils/API/APIURLS"
 import { Section, SectionTitle } from "../../../common/Section/Section";
-import { SectionWrapper, Tile } from "./styled";
+import { NoMovieIcon, SectionWrapper, StyledLink, Tile, TileImage, TileImageContainer, TileTags, TileTag, TileTitle, TileSubTitle, Rating, TileContent } from "./styled";
 import Pagination from '../../../common/Pagination/index';
 import { Container, SpinnerIcon } from '../../../common/Loading/Loading';
 
 function MovieList() {
-    const dispatch = useDispatch();
-    const currentPage = useSelector(selectPageState);
-    const popularMovies = useSelector(selectMovieList);
-    const loading = useSelector(selectLoading);
+	const dispatch = useDispatch();
+	const currentPage = useSelector(selectPageState);
+	const popularMovies = useSelector(selectMovieList);
+	const loading = useSelector(selectLoading);
 
-    const history = useHistory();
-    const location = useLocation();
-    const searchParams = new URLSearchParams(location.search);
-    const query = searchParams.get('page');
+	const history = useHistory();
+	const location = useLocation();
+	const searchParams = new URLSearchParams(location.search);
+	const query = searchParams.get('page');
 
-    useEffect(() => {
-        if (query < 1) {
-            searchParams.set('page', 1);
-        } else {
-            dispatch(pageNumberFromURL(query));
-        }
-    }, [query]);
+	useEffect(() => {
+		if (query < 1) {
+			searchParams.set('page', 1);
+		} else {
+			dispatch(pageNumberFromURL(query));
+		}
+	}, [query]);
 
-    useEffect(() => {
+	useEffect(() => {
 
-        history.push(`${location.pathname}?page=${currentPage}`);
-    }, [currentPage]);
+		history.push(`${location.pathname}?page=${currentPage}`);
+	}, [currentPage]);
 
-    if (loading) {
-        return (
-            <Container>
-                <SpinnerIcon />
-            </Container>
-        );
-    }
-    if (!popularMovies) {
-        return <p>No data available.</p>;
-    }
-    return (
-        <>
-            <Section>
-                <SectionTitle>
-                    Popular Movies
-                </SectionTitle>
-                <SectionWrapper>
-                    {popularMovies.slice(0, 4).map((movie) => (
-                        <li key={movie.id}>
-                            <Tile >
-                                <Link as={Link} to={`/movies/${movie.id}`}>
-                                    <img
-                                        src={movie.poster_path}
-                                        alt={movie.title}
-                                    />
-                                    <p>{movie.title}</p>
-                                    <p>{`⭐ ${movie.vote_average}`}</p>
-                                    <p>{`${movie.vote_count} votes`}</p>
-                                </Link>
-                            </Tile>
-                        </li>
-                    ))}
-                </SectionWrapper>
-            </Section>
-            <Pagination currentPage={currentPage} goToFirstPage={goToFirstPage} incrementPage={incrementPage} decrementPage={decrementPage} />        </>
-    );
+	if (loading) {
+		return (
+			<Container>
+				<SpinnerIcon />
+			</Container>
+		);
+	}
+	if (!popularMovies) {
+		return <p>No data available.</p>;
+	}
+	return (
+		<>
+			<Section>
+				<SectionTitle>
+					Popular Movies
+				</SectionTitle>
+				<SectionWrapper>
+					{popularMovies.slice(0, 4).map((movie) => (
+						<li key={movie.id}>
+								<StyledLink as={Link} to={`/movies/${movie.id}`}>
+									<Tile >
+									<TileImageContainer>
+										{movie.poster_path
+											?
+											<TileImage
+												src={`${posterURL}${movie.poster_path}`}
+												alt=""
+											/>
+											:
+											<NoMovieIcon />
+										}
+									</TileImageContainer>
+									<TileContent>
+									<TileTitle>{movie.title}</TileTitle>
+									<TileSubTitle>{movie.release_date}</TileSubTitle>
+									<TileTags>
+										{
+											movie.genre_ids &&
+											<TileTag>{movie.genre_ids}</TileTag>
+										}
+									</TileTags>
+									<Rating><span>{`⭐ ${movie.vote_average}`}</span>
+									<span>{`${movie.vote_count} votes`}</span></Rating>
+									</TileContent>
+								</Tile>
+								</StyledLink>
+							
+						</li>
+					))}
+				</SectionWrapper>
+			</Section>
+			<Pagination currentPage={currentPage} goToFirstPage={goToFirstPage} incrementPage={incrementPage} decrementPage={decrementPage} />        </>
+	);
 };
 
 export default MovieList;
--- src/features/movies/MovieList/styled.js
+++ src/features/movies/MovieList/styled.js
@@ -1,4 +1,11 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ReactComponent as NoMovieSVG } from "../../../assets/icon-video.svg";
+
+export const StyledLink = styled(Link)`
+	text-decoration: none;
+	color: inherit;
+`
 
 export const SectionWrapper = styled.ul`
 	margin: 0;
@@ -15,11 +22,130 @@ export const SectionWrapper = styled.ul`
 `
 
 export const Tile = styled.div`
-    width: 324px;
-    height: 650px;
+	display: grid;
+	grid-template-rows: auto 1fr;
+	gap: 16px;
+    max-width: 324px;
+    height: 100%;
+	min-height: 650px;
     text-decoration: none;
     color: ${({ theme }) => theme.color.black};
     padding: 16px;
     background-color: ${({ theme }) => theme.color.white};
     box-shadow: 0px 4px 12px 0px rgba(186, 199, 213, 0.5);
+
+	@media (max-width: 767px) {
+		display: grid;
+		grid-template-columns: auto 1fr;
+		grid-template-rows: none;
+		height: auto;
+		min-height: 201px;
+		min-width: 288px;
+	}
+`
+
+export const TileImageContainer = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-shrink: 1;
+	max-width: 292px;
+	height: 434px;
+	border-radius: 5px;
+	background: ${({ theme }) => theme.color.silver};
+	color: ${({ theme }) => theme.color.white};
+
+	@media (max-width: 767px) {
+		width: 114px;
+		height: 169px;
+	}
+`
+
+export const TileImage = styled.img`
+	flex-shrink: 1;
+	max-width: 292px;
+	border-radius: 5px;
+	background-color: ${({ theme }) => theme.color.silver};
+
+	@media (max-width: 767px) {
+		width: 114px;
+	}
+`
+
+export const NoMovieIcon = styled(NoMovieSVG)`
+	width: 72px;
+	height: 72px;
+
+	@media (max-width: 767px) {
+		width: 48px;
+		height: 48px;
+	}
+`
+
+export const TileContent = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 8px;
+`
+
+export const TileTitle = styled.h2`
+	margin: 0;
+	font-size: 22px;
+	font-weight: 500;
+	line-height: 1.3;
+	color: ${({ theme }) => theme.color.almostBlack};
+
+	@media (max-width: 767px) {
+		font-size: 16px;
+	}
+`
+
+export const TileSubTitle = styled.h3`
+	margin: 0;
+	font-size: 16px;
+	font-weight: 400;
+	line-height: 1.5;
+	height: auto;
+	color: ${({ theme }) => theme.color.darkGray};
+
+	@media (max-width: 767px) {
+		font-size: 13px;
+		line-height: 1.3;
+	}
+`
+
+export const TileTags = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	gap: 16px;
+
+	@media (max-width: 767px) {
+		margin: 4px 0;
+		gap: 8px;
+	}
+`
+
+export const TileTag = styled.div`
+	padding: 8px 16px;
+	background-color: ${({ theme }) => theme.color.gray};
+	border-radius: 5px;
+	align-items: center;
+	justify-content: center;
+	color: ${({ theme }) => theme.color.almostBlack};
+	font-size: 14px;
+	line-height: 1;
+
+	@media (max-width: 767px) {
+		padding: 4px 8px;
+		font-size: 10px;
+		line-height: 1.1;
+	}
+`
+
+export const Rating = styled.div`
+	margin-top: auto;
+
+	@media (max-width: 767px) {
+		margin: 0;
+	}
 `
