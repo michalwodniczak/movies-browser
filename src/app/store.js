07f74f256c4a56b1680d1ffc19a3179004c840e1
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-01-05 10:34:46 +0100

    added sagaMiddelwarre

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
-import paginationReducer from "../common/Pagination/paginationSlice"; 
+import createSagaMiddleware from "redux-saga";
+import paginationReducer from "../common/Pagination/paginationSlice";
 import searchReducer from "../Navigation//Search/searchSlice";
+import { watchFetchData } from '../Navigation/Search/searchSaga';
+
+const sagaMiddelaware = createSagaMiddleware();
 
 export const store = configureStore({
     reducer: {
         pagination: paginationReducer,
         search: searchReducer,
-    }
+    },
+    middleware: [sagaMiddelaware],
 });
+
+sagaMiddelaware.run(watchFetchData);
