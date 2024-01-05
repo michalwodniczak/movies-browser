fe80d7c0e00bb2f8fbbbc6ee15f2200e65ac2911
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-01-05 10:37:20 +0100

    added getSearch function to fetch date from api

---
+++ src/Navigation/Search/getSearch.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const getSearch = async (query) => {
+    
+    const apiKey = "f9cdeaf9af4ca30fda858745e2b12712";
+    const URL = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`;
+    
+    try {
+        const response = await axios.get(URL);
+        console.log(response.data.results);
+        return response.data.results
+    } catch {
+        throw new Error("Failed to search");
+    };
+};
+
+//${URL}/search/movie?query=${query}&apiKey=${apiKey}
+
+//  url pod film``
+
+// url pod osobe `https://api.themoviedb.org/3/search/person?query=johnny+depp&api_key=f9cdeaf9af4ca30fda858745e2b12712`
