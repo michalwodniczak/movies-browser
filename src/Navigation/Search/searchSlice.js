6ce37f8f4596a7a05ea342682bb598672de41577
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-01-05 10:37:45 +0100

    added searchSlice

--- src/Navigation/Search/searchSlice.js
+++ src/Navigation/Search/searchSlice.js
@@ -4,17 +4,34 @@ const searchSlice = createSlice({
     name: "search",
     initialState: {
         path: "/",
+        data: null,
+        error: null,
+        loading: false,
     },
     reducers: {
-        setPath:(state, {payload}) => {
-            state.path = payload;
+        setPath: (state, { payload: path }) => {
+            state.path = path;
+        },
+        fetchData: (state) => {
+            state.loading = true;
+            state.error = null;
+        },
+        fetchDataSucces: (state, { payload: data }) => {
+            state.loading = false;
+            state.data = data;
+        },
+        fetchDataFailure: (state, { payload: error }) => {
+            state.data = null;
+            state.error = error
         },
     },
-
 });
 
 export const selectPath = (state) => state.search.path;
+export const selectSearchLoading = (state) => state.search.loading;
+export const selectData = (state) => state.search.data;
+export const selectSearchError = (state) => state.search.error;
 
-export const {setPath} = searchSlice.actions;
+export const { setPath, fetchData, fetchDataFailure, fetchDataSucces } = searchSlice.actions;
 
 export default searchSlice.reducer
