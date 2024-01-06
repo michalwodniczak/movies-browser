7a6171cc14a76b6909fa3629c365e2146db60ec9
Mateusz Bednarz <bednarz.mateusz@gmail.com>
2024-01-06 21:03:56 +0100

    added limit explanation

--- src/utils/pageLimit.js
+++ src/utils/pageLimit.js
@@ -1,1 +1,6 @@
+//As of Jan 2024 TMDB sets an upper-limit on page number one can request from API.
+//Any query for page above #500 gives the following response:
+//"status_message": "Invalid page: Pages start at 1 and max at 500. They are expected to be an integer.",
+//hence a hardcoded integer below.
+
 export default 500;
