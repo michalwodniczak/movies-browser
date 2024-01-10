7c090446ca1f1c2918cdea7de6931ae18dad1178
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-01-10 16:04:04 +0100

    added logic in which we paste our query into the url

--- src/Navigation/Search/index.js
+++ src/Navigation/Search/index.js
@@ -1,15 +1,31 @@
 import { useSelector, useDispatch } from "react-redux";
-import { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation, useHistory } from "react-router-dom";
 import { Wrapper, Input, Icon } from "./styled";
-import { fetchData, selectInputValue, selectPath,setInputValue,setPath} from "./searchSlice";
+import { fetchData, selectInputValue, selectPath, setInputValue, setPath } from "./searchSlice";
 
 export const Search = () => {
     const location = useLocation();
     const dispatch = useDispatch();
     const path = useSelector(selectPath);
     const inputValue = useSelector(selectInputValue);
-    
+    const history = useHistory();
+    const searchParams = new URLSearchParams(location.search);
+    const query = searchParams.get("search");
+
+    useEffect(() => {
+        if (query) {
+            searchParams.set("search", inputValue);
+        }else{
+            dispatch(fetchData());
+            history.push(`${location.pathname}`);
+        }
+    }, [query]);
+
+    useEffect(() => {
+        history.push(`${location.pathname}?search=${inputValue}`);
+    }, [inputValue]);
+
     const onInputChange = ({ target }) => {
         dispatch(setInputValue(target.value));
         dispatch(fetchData({ query: target.value }));
