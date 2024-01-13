21c4b54f27f7ade622df4a2ca642c042ee24314f
Mateusz Bednarz <118743173+bedicooper@users.noreply.github.com>
2024-01-13 11:54:01 +0100

    fixed bugs with tile height and subtitle font-weight

--- src/common/Tile/styled.js
+++ src/common/Tile/styled.js
@@ -59,6 +59,7 @@ export const StyledLink = styled(Link)`
 export const Tile = styled.article`
     max-width: 324px;
     min-height: 650px;
+	height: 100%;
     padding: 16px;
 	display: grid;
 	grid-template-rows: auto 1fr;
@@ -69,10 +70,6 @@ export const Tile = styled.article`
     background-color: ${({ theme }) => theme.color.white};
     box-shadow: 0px 4px 12px 0px rgba(186, 199, 213, 0.5);
     border-radius: 5px;
-
-    @media (max-width: ${({ theme }) => theme.breakpoints.desktop}px){
-        min-height: 100%;
-    }
 
 	@media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
         max-width: 100%;
@@ -232,6 +229,8 @@ export const DetailTileTitle = styled(TileTitle)`
 
 	@media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
         font-size: 16px;
+		font-weight: 500;
+		line-height: 1.3;
 		color: ${({ theme }) => theme.color.almostBlack};
 	}
 `
