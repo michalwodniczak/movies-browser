0ef15e3ad6fd37bd90e345fbbc80ec477f7c1a23
Patrycja Gomza <patrycja.gomza@gmail.com>
2024-01-14 10:51:53 +0100

    update label for date in movieTile condition

--- src/common/Tile/index.js
+++ src/common/Tile/index.js
@@ -157,7 +157,7 @@ export const DetailsTile = (
 						<TileDataContent>
 							<TileDataTitle>
 								{
-									movieTile ? `Production: ` : `Place of birth: `
+									movieTile ? `Release date: ` : `Place of birth: `
 								}
 							</TileDataTitle>
 							{secondData}
