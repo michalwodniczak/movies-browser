8583187a07b723431e76ddacabd0cb24f382c519
Patrycja Gomza <patrycja.gomza@gmail.com>
2024-01-14 13:55:39 +0100

    add navigation links to movies in PersonDetails

--- src/features/people/PeopleDetails/index.js
+++ src/features/people/PeopleDetails/index.js
@@ -11,7 +11,7 @@ import {
 import { DetailsTile, ListTileLarge } from "../../../common/Tile/index";
 import { Section, SectionTitle } from '../../../common/Section/Section';
 import { getGenreList } from "../../movies/MovieList/getGenreList";
-import { LargeListWrapper } from "../../../common/Tile/styled";
+import { LargeListWrapper, StyledLink } from "../../../common/Tile/styled";
 import { Main } from "../../../common/Main/Main";
 
 const PersonDetails = () => {
@@ -86,14 +86,16 @@ const PersonDetails = () => {
                             <LargeListWrapper>
                                 {actorCast.map((cast) => (
                                     <li key={cast.credit_id}>
-                                        <ListTileLarge
-                                            posterPath={cast.backdrop_path}
-                                            title={cast.title}
-                                            subtitle={getReleaseYear(cast.release_date) || "No release year available"}
-                                            tags={nameGenres(cast.genre_ids)}
-                                            voteCount={cast.vote_count}
-                                            ratingValue={formatVote(cast.vote_average)}
-                                        />
+                                        <StyledLink to={`/movies/${cast.id}`}>
+                                            <ListTileLarge
+                                                posterPath={cast.backdrop_path}
+                                                title={cast.title}
+                                                subtitle={getReleaseYear(cast.release_date) || "No release year available"}
+                                                tags={nameGenres(cast.genre_ids)}
+                                                voteCount={cast.vote_count}
+                                                ratingValue={formatVote(cast.vote_average)}
+                                            />
+                                        </StyledLink>
                                     </li>
                                 ))}
                             </LargeListWrapper>
@@ -104,14 +106,16 @@ const PersonDetails = () => {
                             <LargeListWrapper>
                                 {actorCrew.map((crew) => (
                                     <li key={crew.credit_id}>
-                                        <ListTileLarge
-                                            posterPath={crew.backdrop_path}
-                                            title={crew.title}
-                                            subtitle={getReleaseYear(crew.release_date) || "No release year available"}
-                                            tags={nameGenres(crew.genre_ids)}
-                                            voteCount={crew.vote_count}
-                                            ratingValue={formatVote(crew.vote_average)}
-                                        />
+                                        <StyledLink to={`/movies/${crew.id}`}>
+                                            <ListTileLarge
+                                                posterPath={crew.backdrop_path}
+                                                title={crew.title}
+                                                subtitle={getReleaseYear(crew.release_date) || "No release year available"}
+                                                tags={nameGenres(crew.genre_ids)}
+                                                voteCount={crew.vote_count}
+                                                ratingValue={formatVote(crew.vote_average)}
+                                            />
+                                        </StyledLink>
                                     </li>
                                 ))}
                             </LargeListWrapper>
