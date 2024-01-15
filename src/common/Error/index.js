02bcd48a02cc86ad04c6bca8f71d1844bb9cb8f5
Patrycja Gomza <patrycja.gomza@gmail.com>
2024-01-15 10:23:11 +0100

    create and style Error component

---
+++ src/common/Error/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Container, StyledErrorIcon, Header, Instruction, StyledButton } from './styled';
+import { setError } from '../../features/movies/movieSlice';
+
+const Error = () => {
+    const history = useHistory();
+
+    const handleBackToHomepage = () => {
+        setError(null); // Reset the error state before redirecting
+        history.push('/movies');
+    };
+
+    return (
+        <Container>
+            <StyledErrorIcon />
+            <Header>Ooops! Something went wrong...</Header>
+            <Instruction>
+                Please check your network connection<br />
+                and try again
+            </Instruction>
+            <StyledButton onClick={handleBackToHomepage}>
+                Back to homepage
+            </StyledButton>
+        </Container>
+    );
+};
+
+export default Error;
