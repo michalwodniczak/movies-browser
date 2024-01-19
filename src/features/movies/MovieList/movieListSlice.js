972b2120085ddddefb98a4064028cb69e487a582
Patrycja Gomza <patrycja.gomza@gmail.com>
2024-01-19 10:16:32 +0100

    add 'error' state property and implement 'setError' action

--- src/features/movies/MovieList/movieListSlice.js
+++ src/features/movies/MovieList/movieListSlice.js
@@ -8,51 +8,56 @@ const movieListSlice = createSlice({
         genres: [{}],
         loading: true,
         success: false,
+        error: null,
     },
     reducers: {
         incrementPage: (state) => {
-			state.page += 1;
+            state.page += 1;
             state.loading = true;
-		},
-		decrementPage: (state) => {
-			state.page -= 1;
+        },
+        decrementPage: (state) => {
+            state.page -= 1;
             state.loading = true;
-		},
+        },
         goToFirstPage: (state) => {
             state.page = 1;
             state.loading = true;
         },
-        goToLastPage: (state, {payload: lastPage}) => {
+        goToLastPage: (state, { payload: lastPage }) => {
             state.page = +lastPage;
             state.loading = true;
         },
-		pageNumberFromURL: (state, { payload: query }) => {
-			state.page = +query;
+        pageNumberFromURL: (state, { payload: query }) => {
+            state.page = +query;
             state.loading = true;
-		},
+        },
         setMovieList: (state, { payload: movies }) => {
             state.movies = movies;
             state.loading = false;
             state.success = true;
         },
-        setGenres: (state, {payload: genreList}) => {
+        setGenres: (state, { payload: genreList }) => {
             state.genres = genreList.genres;
         },
         setLoading: (state, { payload: loading }) => {
             state.loading = loading;
         },
+        setError: (state, action) => {
+            state.error = action.payload;
+        },
     },
 });
 
-export const { 
+export const {
     incrementPage,
     decrementPage,
     goToFirstPage,
     goToLastPage,
-	pageNumberFromURL,
+    pageNumberFromURL,
     setMovieList,
-    setGenres, 
+    setGenres,
     setLoading,
+    setError,
 } = movieListSlice.actions;
 
 const selectMovieListState = (state) => state.movieList;
@@ -60,5 +65,6 @@ const selectMovieListState = (state) => state.movieList;
 export const selectMovieList = (state) => selectMovieListState(state).movies;
 export const selectLoading = (state) => selectMovieListState(state).loading;
 export const selectPageState = (state) => selectMovieListState(state).page;
+export const selectError = state => selectMovieListState(state).error;
 
 export default movieListSlice.reducer;
