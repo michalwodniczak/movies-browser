3cc3d6707abc61c246ac91edbb6ac2b6983b9a70
Mateusz Bednarz <118743173+bedicooper@users.noreply.github.com>
2024-01-19 13:31:59 +0100

    MovieList: one state property to render Loading/Error/Page

--- src/features/movies/MovieList/index.js
+++ src/features/movies/MovieList/index.js
@@ -8,8 +8,8 @@ import {
 	goToLastPage,
 	pageNumberFromURL,
 	selectMovieList,
-	selectLoading,
-	selectPageState
+	selectPageState,
+	selectStatus
 } from "./movieListSlice";
 import { Main } from "../../../common/Main/Main";
 import { Section, SectionTitle } from "../../../common/Section/Section";
@@ -17,12 +17,13 @@ import { ListTileLarge } from '../../../common/Tile';
 import { StyledLink, LargeListWrapper } from '../../../common/Tile/styled';
 import Pagination from '../../../common/Pagination/index';
 import { Loading } from '../../../common/Loading';
+import Error from '../../../common/Error';
 
 function MovieList() {
 	const dispatch = useDispatch();
 	const currentPage = useSelector(selectPageState);
 	const popularMovies = useSelector(selectMovieList);
-	const loading = useSelector(selectLoading);
+	const status = useSelector(selectStatus);
 
 	const history = useHistory();
 	const location = useLocation();
@@ -41,46 +42,45 @@ function MovieList() {
 		history.push(`${location.pathname}?page=${currentPage}`);
 	}, [currentPage]);
 
-	if (loading) {
-		return (
-			<Loading />
-		);
+	switch (status) {
+		case "loading":
+			return <Loading />;
+		case "error":
+			return <Error />;
+		default:
+			return (
+				<Main>
+					<Section>
+						<SectionTitle>
+							Popular Movies
+						</SectionTitle>
+						<LargeListWrapper>
+							{popularMovies.map((movie) => (
+								<li key={movie.id}>
+									<StyledLink to={`/movies/${movie.id}`}>
+										<ListTileLarge
+											posterPath={movie.posterPath}
+											title={movie.title}
+											subtitle={movie.year}
+											tags={movie.namedGenres}
+											voteCount={movie.votes}
+											ratingValue={movie.rating}
+										/>
+									</StyledLink>
+								</li>
+							))}
+						</LargeListWrapper>
+					</Section>
+					<Pagination
+						currentPage={currentPage}
+						goToFirstPage={goToFirstPage}
+						incrementPage={incrementPage}
+						decrementPage={decrementPage}
+						goToLastPage={goToLastPage}
+					/>
+				</Main>
+			)
 	}
-	if (!popularMovies) {
-		return <p>No data available.</p>;
-	}
-	return (
-		<Main>
-			<Section>
-				<SectionTitle>
-					Popular Movies
-				</SectionTitle>
-				<LargeListWrapper>
-					{popularMovies.map((movie) => (
-						<li key={movie.id}>
-							<StyledLink to={`/movies/${movie.id}`}>
-								<ListTileLarge
-									posterPath={movie.posterPath}
-									title={movie.title}
-									subtitle={movie.year}
-									tags={movie.namedGenres}
-									voteCount={movie.votes}
-									ratingValue={movie.rating}
-								/>
-							</StyledLink>
-						</li>
-					))}
-				</LargeListWrapper>
-			</Section>
-			<Pagination
-				currentPage={currentPage}
-				goToFirstPage={goToFirstPage}
-				incrementPage={incrementPage}
-				decrementPage={decrementPage}
-				goToLastPage={goToLastPage}
-			/>
-		</Main>
-	);
 };
 
 export default MovieList;
