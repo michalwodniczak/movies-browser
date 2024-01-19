3cc62c59ad0053f9ca86851164123e34b262c24e
Patrycja Gomza <patrycja.gomza@gmail.com>
2024-01-19 11:53:05 +0100

    remove unused import and exclude the useHistory hook

--- src/common/AnimatedPage.js
+++ src/common/AnimatedPage.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { useSpring, animated } from 'react-spring';
-import { useHistory } from 'react-router-dom';
 
 const AnimatedPage = ({ children }) => {
-    const history = useHistory();
-
     const props = useSpring({
         opacity: 1,
         from: { opacity: 0 },
