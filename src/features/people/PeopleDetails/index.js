524b644724bf1b8e4921c8806fd160bc9ac5d6c5
Mateusz Bednarz <118743173+bedicooper@users.noreply.github.com>
2024-01-19 13:32:50 +0100

    PeopleDetails: one state property to render Loading/Error/Page

--- src/features/people/PeopleDetails/index.js
+++ src/features/people/PeopleDetails/index.js
@@ -28,65 +28,68 @@ const PersonDetails = () => {
 	const actorCast = useSelector(selectPersonCast);
 	const actorCrew = useSelector(selectPersonCrew);
 
-	return (
-		status === "loading" ? <Loading />
-			: status === "error" ? <Error />
-				: details && (
-					<Main>
-						<DetailsTile
-							posterPath={details.posterPath}
-							title={details.name}
-							description={details.biography}
-							firstData={details.birthday || "No date of birth available"}
-							secondData={details.birthplace || "No place of birth available"}
-						/>
-						<Section>
-							<SectionTitle>
-								{`Movies - cast (${actorCast.length})`}
-							</SectionTitle>
-							<LargeListWrapper>
-								{actorCast.map((cast) => (
-									<li key={cast.creditID}>
-										<StyledLink to={`/movies/${cast.id}`}>
-											<ListTileLarge
-												posterPath={cast.posterPath}
-												title={cast.title}
-												role={cast.role}
-												subtitle={cast.year || "No release year available"}
-												tags={cast.namedGenres}
-												voteCount={cast.votes}
-												ratingValue={cast.rating}
-											/>
-										</StyledLink>
-									</li>
-								))}
-							</LargeListWrapper>
-						</Section>
-						<Section>
-							<SectionTitle>
-								{`Movies - crew (${actorCrew.length})`}
-							</SectionTitle>
-							<LargeListWrapper>
-								{actorCrew.map((crew) => (
-									<li key={crew.creditID}>
-										<StyledLink to={`/movies/${crew.id}`}>
-											<ListTileLarge
-												posterPath={crew.posterPath}
-												title={crew.title}
-												role={crew.role}
-												subtitle={crew.year || "No release year available"}
-												tags={crew.namedGenres}
-												voteCount={crew.votes}
-												ratingValue={crew.rating}
-											/>
-										</StyledLink>
-									</li>
-								))}
-							</LargeListWrapper>
-						</Section>
-					</Main>
-				)
-	)
+	switch (status) {
+		case "loading":
+			return <Loading />;
+		case "error":
+			return <Error />;
+		default:
+			return (
+				<Main>
+					<DetailsTile
+						posterPath={details.posterPath}
+						title={details.name}
+						description={details.biography}
+						firstData={details.birthday || "No date of birth available"}
+						secondData={details.birthplace || "No place of birth available"}
+					/>
+					<Section>
+						<SectionTitle>
+							{`Movies - cast (${actorCast.length})`}
+						</SectionTitle>
+						<LargeListWrapper>
+							{actorCast.map((cast) => (
+								<li key={cast.creditID}>
+									<StyledLink to={`/movies/${cast.id}`}>
+										<ListTileLarge
+											posterPath={cast.posterPath}
+											title={cast.title}
+											role={cast.role}
+											subtitle={cast.year || "No release year available"}
+											tags={cast.namedGenres}
+											voteCount={cast.votes}
+											ratingValue={cast.rating}
+										/>
+									</StyledLink>
+								</li>
+							))}
+						</LargeListWrapper>
+					</Section>
+					<Section>
+						<SectionTitle>
+							{`Movies - crew (${actorCrew.length})`}
+						</SectionTitle>
+						<LargeListWrapper>
+							{actorCrew.map((crew) => (
+								<li key={crew.creditID}>
+									<StyledLink to={`/movies/${crew.id}`}>
+										<ListTileLarge
+											posterPath={crew.posterPath}
+											title={crew.title}
+											role={crew.role}
+											subtitle={crew.year || "No release year available"}
+											tags={crew.namedGenres}
+											voteCount={crew.votes}
+											ratingValue={crew.rating}
+										/>
+									</StyledLink>
+								</li>
+							))}
+						</LargeListWrapper>
+					</Section>
+				</Main>
+			)
+	}
 };
 
 export default PersonDetails;
