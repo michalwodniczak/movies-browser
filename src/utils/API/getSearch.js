5168516a78120fe0e72544528bfe040b21b0e4a1
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-01-21 15:00:09 +0100

    refactored and changed path for getSearch

---
+++ src/utils/API/getSearch.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export const getSearch = async (query, path) => {
+
+    const searchPath = () => {
+        switch(path){
+            case "movies":
+                return "movie";
+            case "people":
+                return "person";
+        }
+    }
+
+    const apiKey = "f9cdeaf9af4ca30fda858745e2b12712";
+    const URL = `https://api.themoviedb.org/3/search/${searchPath()}?api_key=${apiKey}&query=${query}`;
+
+
+    try {
+        const response = await axios.get(URL);
+        return response.data;
+    } catch {
+        throw new Error("Failed to search");
+    };
+};
