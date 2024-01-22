35663a80d66cd5ab466abb2a51af85084454045c
Mateusz Bednarz <118743173+bedicooper@users.noreply.github.com>
2024-01-22 19:27:17 +0100

    added SearchPage index and styled files, utilized new components

---
+++ src/features/SearchPage/index.js
@@ -0,0 +1,48 @@
+import { useSelector } from "react-redux";
+import { 
+    selectData, 
+    selectInputValue, 
+    selectPath, 
+    selectStatus } from "./searchSlice";
+import { NoResult } from "./NoResults";
+import { SearchedMovies } from "./SearchedMovies";
+import { SearchedPeople } from "./SearchedPeople";
+import { Loading } from "../../common/Loading";
+import Error from "../../common/Error";
+
+export const SearchPage = () => {
+    const searchQuery = useSelector(selectInputValue);
+    const searchResults = useSelector(selectData);
+    const path = useSelector(selectPath);
+    const status = useSelector(selectStatus);
+
+    if (!searchResults.length && status !== "loading") {
+        return (
+            <NoResult
+                searchQuery={searchQuery}
+            />
+        );
+    }
+
+    switch (status) {
+        case "loading":
+            return <Loading />;
+        case "error":
+            return <Error />;
+        default:
+
+            if (path === "movies") {
+                return <SearchedMovies
+                    searchQuery={searchQuery}
+                    searchResults={searchResults}
+                />
+            }
+
+            if (path === "people") {
+                return <SearchedPeople
+                    searchQuery={searchQuery}
+                    searchResults={searchResults}
+                />
+            }
+    }
+};
