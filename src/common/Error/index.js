74699c47b3fb654a4956a10be138d4cd4065a325
Patrycja Gomza <patrycja.gomza@gmail.com>
2024-01-22 15:08:20 +0100

    improve error handling and page reloading in Error

--- src/common/Error/index.js
+++ src/common/Error/index.js
@@ -2,19 +2,12 @@ import React from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import { Container, StyledErrorIcon, Header, Instruction, StyledButton } from './styled';
 
-const Error = ({ setError }) => {
+const Error = ({ error }) => {
     const history = useHistory();
     const location = useLocation();
 
     const handleBackToHomepage = () => {
-        setError(null); // Reset the error state before redirecting
-
-        // Check if the current location is MovieList, then use "reload" on the button
-        if (location.pathname === '/movies') {
-            window.location.reload();
-        } else {
-            history.push('/movies');
-        }
+        history.push('/movies?page=1');
     };
 
     return (
@@ -22,6 +15,7 @@ const Error = ({ setError }) => {
             <StyledErrorIcon />
             <Header>Ooops! Something went wrong...</Header>
             <Instruction>
+                {error.message}<br />
                 Please check your network connection<br />
                 and try again
             </Instruction>
