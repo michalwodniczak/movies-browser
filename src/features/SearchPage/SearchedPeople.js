a341c02d23a7f37e1d46447889f60000c2ac0e9b
Mateusz Bednarz <118743173+bedicooper@users.noreply.github.com>
2024-01-22 21:02:56 +0100

    pagination added to SearchedPeople page

--- src/features/SearchPage/SearchedPeople.js
+++ src/features/SearchPage/SearchedPeople.js
@@ -1,10 +1,23 @@
+import { useSelector } from "react-redux";
 import { Main } from "../../common/Main/Main";
 import { Section, SectionTitle } from "../../common/Section/Section";
 import { StyledLink } from "../../common/Tile/styled";
 import { ListTileSmall } from "../../common/Tile";
 import { SmallListWrapper } from "../../common/Tile/styled";
+import Pagination from '../../common/Pagination';
+import {
+    incrementPage,
+    decrementPage,
+    goToFirstSearchPage,
+    goToLastSearchPage,
+    selectCurrnetPage,
+    selectTotalPages,
+} from '../SearchPage/searchSlice'
 
 export const SearchedPeople = ({ searchQuery, searchResults, totalResults }) => {
+    const result = searchResults;
+    const currentPage = useSelector(selectCurrnetPage);
+    const totalPages = useSelector(selectTotalPages);
 
     return (
         <Main>
@@ -23,6 +36,14 @@ export const SearchedPeople = ({ searchQuery, searchResults, totalResults }) => 
                     ))}
                 </SmallListWrapper>
             </Section>
+            <Pagination
+                currentPage={currentPage}
+                goToFirstPage={goToFirstSearchPage}
+                incrementPage={incrementPage}
+                decrementPage={decrementPage}
+                goToLastPage={goToLastSearchPage}
+                totalPages={totalPages}
+            />
         </Main>
     );
 };
