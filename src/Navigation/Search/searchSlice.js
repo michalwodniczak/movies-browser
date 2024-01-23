b053b349dc99e49c885124b7a22f70f6f6c26f16
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-01-23 18:03:45 +0100

    changed searchSlice , added new initialState property and new reducers

--- src/Navigation/Search/searchSlice.js
+++ src/Navigation/Search/searchSlice.js
@@ -3,51 +3,87 @@ import { createSlice } from "@reduxjs/toolkit";
 const searchSlice = createSlice({
     name: "search",
     initialState: {
-        path: "/",
-        // data: null,
-        error: null,
-        loading: false,
+        path: "",
+        inputValue: "",
+        data: null,
+        status: "",
         genres: [{}],
-        movies: [{}]
+        totalResults: 0,
+        totalPages: 1,
+        page: 1,
     },
     reducers: {
         setPath: (state, { payload: path }) => {
             state.path = path;
         },
-        setInputValue: (state, { payload: value }) => {
-            state.inputValue = value;
+        setLoading: (state, { payload: data }) => {
+            state.status = "loading"
+            state.data = data;
         },
         setGenres: (state, { payload: genresList }) => {
             state.genres = genresList.genres;
         },
-
-        setMoviesList: (state, { payload: movies }) => {
-            state.loading = false;
-            state.movies = movies;
+        setTotalResults: (state, { payload: result }) => {
+            state.totalResults = result.total_results;
         },
-
-        fetchData: (state) => {
-            state.loading = true;
-            state.error = null;
+        setTotalPages: (state, { payload: result }) => {
+            state.totalPages = result.total_pages;
         },
         fetchDataSucces: (state, { payload: data }) => {
-            state.loading = false;
+            state.status = "success";
             state.data = data;
         },
-        fetchDataFailure: (state, { payload: error }) => {
-            state.loading = false;
+        fetchDataFailure: (state, { payload: message }) => {
             state.data = null;
-            state.error = error
+            state.status = "error";
+            state.error = message;
         },
+        setInputValue: (state, { payload: query }) => {
+            state.status = "loading"
+            state.inputValue = query;
+        },
+        incrementPage: (state) => {
+            state.page += 1;
+            state.status = "loading";
+        },
+        decrementPage: (state) => {
+            state.page -= 1;
+            state.status = "loading";
+        },
+        goToFirstSearchPage: (state) => {
+            state.page = 1;
+            state.status = "loading";
+        },
+        goToLastSearchPage: (state, { payload: lastPage }) => {
+            state.page = +lastPage;
+            state.status = "loading";
+        }
     },
 });
 
-export const selectPath = (state) => state.search.path;
-export const selectSearchLoading = (state) => state.search.loading;
-export const selectData = (state) => state.search.movies;
-export const selectSearchError = (state) => state.search.error;
-export const selectInputValue = (state) => state.search.inputValue;
+const selectSearchState = (state) => state.search;
 
-export const { setPath, setGenres, setMoviesList, fetchData, fetchDataFailure, fetchDataSucces, setInputValue } = searchSlice.actions;
+export const selectPath = (state) => selectSearchState(state).path;
+export const selectData = (state) => selectSearchState(state).data;
+export const selectInputValue = (state) => selectSearchState(state).inputValue;
+export const selectStatus = (state) => selectSearchState(state).status;
+export const selectCurrnetPage = (state) => selectSearchState(state).page;
+export const selectTotalPages = (state) => selectSearchState(state).totalPages;
+export const selectTotalResults = (state) => selectSearchState(state).totalResults;
 
-export default searchSlice.reducer
+export const {
+    setPath,
+    fetchDataFailure,
+    fetchDataSucces,
+    setInputValue,
+    setGenres,
+    setLoading,
+    setTotalPages,
+    setTotalResults,
+    incrementPage,
+    decrementPage,
+    goToFirstSearchPage,
+    goToLastSearchPage,
+} = searchSlice.actions;
+
+export default searchSlice.reducer;
