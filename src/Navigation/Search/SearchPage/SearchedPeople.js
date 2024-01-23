6cbebc2e295ff6bd2317bb0c7ca4a69230cfb907
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-01-23 19:26:52 +0100

    added Component SearchedPeople

---
+++ src/Navigation/Search/SearchPage/SearchedPeople.js
@@ -0,0 +1,44 @@
+import { useSelector } from "react-redux";
+import { Main } from "../../../common/Main/Main";
+import { Section, SectionTitle } from "../../../common/Section/Section";
+import { SmallListWrapper, StyledLink } from "../../../common/Tile/styled";
+import { ListTileSmall, } from "../../../common/Tile";
+import Pagination from "../../../common/Pagination";
+import { selectCurrnetPage, selectTotalPages } from "../searchSlice";
+import { goToFirstSearchPage, goToLastSearchPage, incrementPage, decrementPage } from "../searchSlice";
+
+
+
+export const SearchedPeople = ({ searchQuery, searchResults, totalResults }) => {
+    const result = searchResults;
+    const currentPage = useSelector(selectCurrnetPage);
+    const totalPages = useSelector(selectTotalPages);
+  
+    return (
+        <Main>
+            <Section>
+                <SectionTitle>{`Search result for "${searchQuery}" (${totalResults})`}</SectionTitle>
+                <SmallListWrapper>
+                    {result.map((person) => (
+                        <li key={person.id}>
+                            <StyledLink to={`/people/${person.id}`}>
+                                <ListTileSmall
+                                    posterPath={person.profile_path}
+                                    title={person.name}
+                                />
+                            </StyledLink>
+                        </li>
+                    ))}
+                </SmallListWrapper>
+            </Section>
+            <Pagination
+                currentPage={currentPage}
+                goToFirstPage={goToFirstSearchPage}
+                incrementPage={incrementPage}
+                decrementPage={decrementPage}
+                goToLastPage={goToLastSearchPage}
+                totalPages={totalPages}
+            />
+        </Main>
+    )
+}
