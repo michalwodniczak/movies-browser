9f2e4133669d195a19205375d60f50744ccdeb87
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-01-23 19:39:17 +0100

    added AnimatedPage on  SearchPeople

--- src/Navigation/Search/SearchPage/SearchedPeople.js
+++ src/Navigation/Search/SearchPage/SearchedPeople.js
@@ -6,6 +6,7 @@ import { ListTileSmall, } from "../../../common/Tile";
 import Pagination from "../../../common/Pagination";
 import { selectCurrnetPage, selectTotalPages } from "../searchSlice";
 import { goToFirstSearchPage, goToLastSearchPage, incrementPage, decrementPage } from "../searchSlice";
+import AnimatedPage from "../../../common/AnimatedPage";
 
 
 
@@ -13,32 +14,34 @@ export const SearchedPeople = ({ searchQuery, searchResults, totalResults }) => 
     const result = searchResults;
     const currentPage = useSelector(selectCurrnetPage);
     const totalPages = useSelector(selectTotalPages);
-  
+
     return (
-        <Main>
-            <Section>
-                <SectionTitle>{`Search result for "${searchQuery}" (${totalResults})`}</SectionTitle>
-                <SmallListWrapper>
-                    {result.map((person) => (
-                        <li key={person.id}>
-                            <StyledLink to={`/people/${person.id}`}>
-                                <ListTileSmall
-                                    posterPath={person.profile_path}
-                                    title={person.name}
-                                />
-                            </StyledLink>
-                        </li>
-                    ))}
-                </SmallListWrapper>
-            </Section>
-            <Pagination
-                currentPage={currentPage}
-                goToFirstPage={goToFirstSearchPage}
-                incrementPage={incrementPage}
-                decrementPage={decrementPage}
-                goToLastPage={goToLastSearchPage}
-                totalPages={totalPages}
-            />
-        </Main>
+        <AnimatedPage>
+            <Main>
+                <Section>
+                    <SectionTitle>{`Search result for "${searchQuery}" (${totalResults})`}</SectionTitle>
+                    <SmallListWrapper>
+                        {result.map((person) => (
+                            <li key={person.id}>
+                                <StyledLink to={`/people/${person.id}`}>
+                                    <ListTileSmall
+                                        posterPath={person.profile_path}
+                                        title={person.name}
+                                    />
+                                </StyledLink>
+                            </li>
+                        ))}
+                    </SmallListWrapper>
+                </Section>
+                <Pagination
+                    currentPage={currentPage}
+                    goToFirstPage={goToFirstSearchPage}
+                    incrementPage={incrementPage}
+                    decrementPage={decrementPage}
+                    goToLastPage={goToLastSearchPage}
+                    totalPages={totalPages}
+                />
+            </Main>
+        </AnimatedPage>
     )
 }
