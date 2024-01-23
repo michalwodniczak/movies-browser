81897c02526e78be2104a4430e3836ecd2197176
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-01-23 18:11:06 +0100

    deleted useSelector

--- src/Navigation/Search/NoResults.js
+++ src/Navigation/Search/NoResults.js
@@ -1,17 +1,14 @@
-import { useSelector } from "react-redux";
 import { Main } from "../../common/Main/Main";
 import { Section, SectionTitle } from "../../common/Section/Section";
 import { Wrapper } from "./SearchPage/styled";
 import { ReactComponent as IconEmpty } from "../../assets/icon-empty.svg";
-import { selectInputValue } from "./searchSlice";
 
-export const NoResults = () => {
-    const searchQuery = useSelector(selectInputValue);
+export const NoResults = ({ searchQuery }) => {
 
     return (
         <Main>
             <Section>
-                <SectionTitle>Sorry, there are no results for "{searchQuery}"</SectionTitle>
+                <SectionTitle>{`Sorry, there are no results for "${searchQuery}"`}</SectionTitle>
                 <Wrapper>
                     <IconEmpty />
                 </Wrapper>
