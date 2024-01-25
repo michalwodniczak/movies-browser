71f16d5e2abdfa38ea4a5825168c2e2c7680d996
Patrycja Gomza <patrycja.gomza@gmail.com>
2024-01-25 13:13:33 +0100

    add conditional rendering for actorCast and actorCrew

--- src/features/people/PeopleDetails/index.js
+++ src/features/people/PeopleDetails/index.js
@@ -72,45 +72,49 @@ const PersonDetails = () => {
               <SectionTitle>
                 {`Movies - cast (${actorCast.length})`}
               </SectionTitle>
-              <LargeListWrapper>
-                {actorCast.map((cast) => (
-                  <li key={cast.creditID}>
-                    <StyledLink to={`/movies/${cast.id}`}>
-                      <ListTileLarge
-                        posterPath={cast.posterPath}
-                        title={cast.title}
-                        role={cast.role}
-                        subtitle={cast.year || "No release year available"}
-                        tags={cast.namedGenres}
-                        voteCount={cast.votes}
-                        ratingValue={cast.rating}
-                      />
-                    </StyledLink>
-                  </li>
-                ))}
-              </LargeListWrapper>
+              {actorCast && actorCast.length > 0 && (
+                <LargeListWrapper>
+                  {actorCast.map((cast) => (
+                    <li key={cast.creditID}>
+                      <StyledLink to={`/movies/${cast.id}`}>
+                        <ListTileLarge
+                          posterPath={cast.posterPath}
+                          title={cast.title}
+                          role={cast.role}
+                          subtitle={cast.year || "No release year available"}
+                          tags={cast.namedGenres}
+                          voteCount={cast.votes}
+                          ratingValue={cast.rating}
+                        />
+                      </StyledLink>
+                    </li>
+                  ))}
+                </LargeListWrapper>
+              )}
             </Section>
             <Section>
               <SectionTitle>
                 {`Movies - crew (${actorCrew.length})`}
               </SectionTitle>
-              <LargeListWrapper>
-                {actorCrew.map((crew) => (
-                  <li key={crew.creditID}>
-                    <StyledLink to={`/movies/${crew.id}`}>
-                      <ListTileLarge
-                        posterPath={crew.posterPath}
-                        title={crew.title}
-                        role={crew.role}
-                        subtitle={crew.year || "No release year available"}
-                        tags={crew.namedGenres}
-                        voteCount={crew.votes}
-                        ratingValue={crew.rating}
-                      />
-                    </StyledLink>
-                  </li>
-                ))}
-              </LargeListWrapper>
+              {actorCrew && actorCrew.length > 0 && (
+                <LargeListWrapper>
+                  {actorCrew.map((crew) => (
+                    <li key={crew.creditID}>
+                      <StyledLink to={`/movies/${crew.id}`}>
+                        <ListTileLarge
+                          posterPath={crew.posterPath}
+                          title={crew.title}
+                          role={crew.role}
+                          subtitle={crew.year || "No release year available"}
+                          tags={crew.namedGenres}
+                          voteCount={crew.votes}
+                          ratingValue={crew.rating}
+                        />
+                      </StyledLink>
+                    </li>
+                  ))}
+                </LargeListWrapper>
+              )}
             </Section>
           </Main>
         </AnimatedPage>
