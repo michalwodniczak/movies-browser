d1869658a4c4a02a51fc032929ff6428118fc736
Mateusz Bednarz <118743173+bedicooper@users.noreply.github.com>
2024-01-29 14:02:31 +0100

    refactored search/index for input to update URL

--- src/common/Navigation/Search/index.js
+++ src/common/Navigation/Search/index.js
@@ -1,70 +1,54 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 import { usePathname } from "./usePathname";
 import { Wrapper, Input, Icon } from "./styled";
 import {
-    selectInputValue,
     selectPath,
     setInputValue,
-    selectCurrnetPage
 } from "./searchSlice";
 import {
     useURLParameter,
     useUpdatePageFromURL,
-    useUpdateQueryFromURL,
-    useReplaceQueryParameter
 } from "../../../utils/useURLParams";
 import paginationParamName from "../../../utils/paginationParamName";
 import queryParamName from "../../../utils/queryParamName";
 
 export const Search = () => {
     const location = useLocation();
+    const history = useHistory();
     const dispatch = useDispatch();
     const updatePath = usePathname();
-
+    const updatePageFromURL = useUpdatePageFromURL();
+    
     const path = useSelector(selectPath);
-    const inputValue = useSelector(selectInputValue);
-    const currentPage = useSelector(selectCurrnetPage);
-
     const pageParam = useURLParameter(paginationParamName);
     const query = useURLParameter(queryParamName);
-    const replaceQueryParameter = useReplaceQueryParameter();
-    const updateQueryFromURL = useUpdateQueryFromURL();
-    const updatePageFromURL = useUpdatePageFromURL();
-
-    const params = {
-        pageKey: paginationParamName,
-        pageValue: currentPage,
-        queryKey: queryParamName,
-        queryValue: inputValue,
-    };
+    const searchParams = new URLSearchParams(location.search);
 
     useEffect(() => {
         updatePath();
-    }, [location.pathname]);
 
-    useEffect(() => {
-        updatePageFromURL({
-            key: "search",
-            value: +pageParam,
-        });
-    }, [pageParam]);
-
-    useEffect(() => {
-        updateQueryFromURL(query)
-    }, [query]);
-  
-    useEffect(() => {
-        replaceQueryParameter(params);
-    }, [inputValue, currentPage, query, pageParam]);
+        if (query) {
+            updatePageFromURL({
+                key: queryParamName,
+                value: +pageParam,
+            });
+            dispatch(setInputValue(query));
+        };
+    }, [location]);
 
     const onInputChange = ({ target }) => {
-        const trimmedValue = target.value.trim();
+        const inputQuery = target.value.trim() !== 0 ? target.value : undefined;
 
-        if (trimmedValue.length !== 0) {
-            dispatch(setInputValue(target.value));
-        } else dispatch(setInputValue(trimmedValue));
+        if (!inputQuery) {
+            searchParams.delete(queryParamName);
+        } else {
+            searchParams.set(queryParamName, inputQuery);
+            searchParams.set(paginationParamName, "1")
+        }
+
+        return history.push(`${location.pathname}?${searchParams.toString()}`);
     };
 
     return (
@@ -73,7 +57,7 @@ export const Search = () => {
             <Input
                 placeholder={`Search for ${path}...`}
                 onChange={onInputChange}
-                value={inputValue || ""}
+                value={query || ""}
             />
         </Wrapper>
     );
