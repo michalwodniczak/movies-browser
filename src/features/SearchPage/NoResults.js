e5d51e360b57456c3928d07c5edc66a0bf548685
Mateusz Bednarz <118743173+bedicooper@users.noreply.github.com>
2024-01-29 14:30:43 +0100

    added a message to NoResults for queres above page limit

--- src/features/SearchPage/NoResults.js
+++ src/features/SearchPage/NoResults.js
@@ -1,14 +1,26 @@
+import { useSelector } from "react-redux";
+import {
+    selectCurrnetPage,
+    selectTotalPages,
+} from "../../common/Navigation/Search/searchSlice";
 import { Main } from "../../common/Main/Main";
 import { Section, SectionTitle } from "../../common/Section/Section";
 import { Wrapper } from "./styled";
 import { ReactComponent as IconEmpty } from "../../assets/icon-empty.svg";
 
 export const NoResults = ({ searchQuery }) => {
+    const currentPage = useSelector(selectCurrnetPage);
+    const totalPages = useSelector(selectTotalPages);
 
     return (
         <Main>
             <Section>
-                <SectionTitle>{`Sorry, there are no results for "${searchQuery}"`}</SectionTitle>
+                <SectionTitle>
+                    {currentPage > totalPages
+                        ? `Sorry, there are only ${totalPages} pages with results for "${searchQuery}"`
+                        : `Sorry, there are no results for "${searchQuery}"`
+                    }
+                </SectionTitle>
                 <Wrapper>
                     <IconEmpty />
                 </Wrapper>
