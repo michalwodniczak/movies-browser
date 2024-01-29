b82173f28632afd9f2f2619542e40333f496e390
Mateusz Bednarz <118743173+bedicooper@users.noreply.github.com>
2024-01-29 14:35:56 +0100

    code formatting and using page fomr URL in Searched Movies and People

--- src/features/SearchPage/SearchedMovies.js
+++ src/features/SearchPage/SearchedMovies.js
@@ -1,48 +1,53 @@
 import { useSelector } from "react-redux";
+import {
+  selectTotalPages,
+  searchPageNumberFromURL
+} from "../../common/Navigation/Search/searchSlice";
 import { Main } from "../../common/Main/Main";
 import { Section, SectionTitle } from "../../common/Section/Section";
 import { LargeListWrapper, StyledLink } from "../../common/Tile/styled";
 import { ListTileLarge } from "../../common/Tile/index";
-import AnimatedPage from "../../common/AnimatedPage";
 import Pagination from "../../common/Pagination";
-import { incrementPage, decrementPage, goToFirstSearchPage, goToLastSearchPage, selectCurrnetPage, selectTotalPages } from "../../common/Navigation/Search/searchSlice";
+import AnimatedPage from "../../common/AnimatedPage";
 
-export const SearchedMovies = ({ searchQuery, searchResults, totalResults }) => {
-    const result = searchResults;
-    const currentPage = useSelector(selectCurrnetPage);
-    const totalPages = useSelector(selectTotalPages);
+export const SearchedMovies = (
+  {
+    searchQuery,
+    searchResults,
+    totalResults
+  }) => {
+  const result = searchResults;
+  const totalPages = useSelector(selectTotalPages);
 
-    return (
-        <AnimatedPage>
-            <Main>
-                <Section>
-                    <SectionTitle>{`Search result for "${searchQuery}" (${totalResults})`}</SectionTitle>
-                    <LargeListWrapper>
-                        {result.map((movie) => (
-                            <li key={movie.id}>
-                                <StyledLink to={`/movies/${movie.id}`}>
-                                    <ListTileLarge
-                                        posterPath={movie.posterPath}
-                                        title={movie.title}
-                                        subtitle={movie.year}
-                                        tags={movie.namedGenres}
-                                        voteCount={movie.votes}
-                                        ratingValue={movie.rating}
-                                    />
-                                </StyledLink>
-                            </li>
-                        ))}
-                    </LargeListWrapper>
-                </Section>
-                <Pagination
-                    currentPage={currentPage}
-                    goToFirstPage={goToFirstSearchPage}
-                    incrementPage={incrementPage}
-                    decrementPage={decrementPage}
-                    goToLastPage={goToLastSearchPage}
-                    totalPages={totalPages}
-                />
-            </Main>
-        </AnimatedPage>
-    );
+  return (
+    <AnimatedPage>
+      <Main>
+        <Section>
+          <SectionTitle>
+            {`Search result for "${searchQuery}" (${totalResults})`}
+          </SectionTitle>
+          <LargeListWrapper>
+            {result.map((movie) => (
+              <li key={movie.id}>
+                <StyledLink to={`/movies/${movie.id}`}>
+                  <ListTileLarge
+                    posterPath={movie.posterPath}
+                    title={movie.title}
+                    subtitle={movie.year}
+                    tags={movie.namedGenres}
+                    voteCount={movie.votes}
+                    ratingValue={movie.rating}
+                  />
+                </StyledLink>
+              </li>
+            ))}
+          </LargeListWrapper>
+        </Section>
+        <Pagination
+          totalPages={totalPages}
+          pageNumberFromURL={searchPageNumberFromURL}
+        />
+      </Main>
+    </AnimatedPage>
+  );
 };
