f19cacc80012b69e3ed16182e7fb403ad6f88be3
Mateusz Bednarz <118743173+bedicooper@users.noreply.github.com>
2024-01-29 15:04:09 +0100

    removed unused pagination reducers form export

--- src/common/Pagination/createPaginationActions.js
+++ src/common/Pagination/createPaginationActions.js
@@ -1,18 +1,5 @@
 export const createPaginationActions = () => {
 	const paginationReducers = {
-		incrementPage: (state) => {
-			state.page += 1;
-		},
-		decrementPage: (state) => {
-			state.page -= 1;
-		},
-		goToFirstPage: (state) => {
-			state.page = 1;
-		},
-		goToLastPage: (state, { payload: lastPage }) => {
-            state.page = +lastPage;
-            state.status = "loading";
-        },
         pageNumberFromURL: (state, { payload: query }) => {
             state.page = +query;
             state.status = "loading";
